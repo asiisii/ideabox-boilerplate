e432d07f095dc2d6dae0914afcd1f5ed2b77257c
Ashish Malla <ashishmalla45@gmail.com>
2021-02-28 17:54:54 -0700

    Add iteration 4 localstorage part

--- src/idea.js
+++ src/idea.js
@@ -1,13 +1,30 @@
 class Idea {
-  constructor(obj) {
+  constructor(title, body) {
     this.id = Date.now();
-    this.title = obj.title;
-    this.body = obj.body;
+    this.title = title;
+    this.body = body;
     this.star = false;
   }
   saveToStorage() {
     localStorage.setItem("savedIdeas", JSON.stringify(saveIdeaCards));
   }
-  deleteFromStorage() {}
-  updateIdea() {}
+  deleteFromStorage() {
+    localStorage.removeItem('savedIdeas');
+    this.saveToStorage();
+  }
+  updateIdea() {
+    var getId = event.target.closest('.star');
+    var newGetId = parseInt(getId.id);
+    for (var i = 0; i < saveIdeaCards.length; i++) {
+      if (newGetId === saveIdeaCards[i].id) {
+        if (!saveIdeaCards[i].star) {
+          saveIdeaCards[i].star = true;
+          getId.src = "./assets/star-active.svg";
+        } else if (saveIdeaCards[i].star) {
+          saveIdeaCards[i].star = false;
+          getId.src = "./assets/star.svg";
+        }
+      }
+    }
+  }
 }
